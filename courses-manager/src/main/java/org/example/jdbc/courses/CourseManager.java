154fe08c8731ed3647b3667fe637e16fdb972eb4
adamlagoda <adam_lagoda@interia.pl>
2020-07-09 22:53:49 +0200

    PreparedStatement tasks

---
+++ courses-manager/src/main/java/org/example/jdbc/courses/CourseManager.java
@@ -0,0 +1,37 @@
+package org.example.jdbc.courses;
+
+public class CourseManager {
+    /**
+     * Wstawia do tabeli sda_courses.students rekord z imieniem studenta, id kursu na jaki jest zapisany,
+     * krótkim opisem i miejscem, na którym siedzi
+     */
+    public void insertStudent() {
+
+    }
+
+    /**
+     * Usuwa kursy, zakończone przed określoną datą
+     */
+    public void deleteCoursesFinishedBefore() {
+
+    }
+
+    /**
+     * Dodaje do listy obecności z jakiegoś dnia, na kursie o określonym course_id, studenta o określonym student_id
+     */
+    public void insertAttendance() {
+
+    }
+
+    /**
+     * Aktualizuje miejsce, w którym będzie odbywał się kurs
+     */
+
+    public void updateCoursePlace() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
