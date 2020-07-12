9ed4344df0a99dff63c743a2d3b93936635f2e26
adamlagoda <adam_lagoda@interia.pl>
2020-07-12 10:34:47 +0200

    ConnectionFactory

--- jdbc-starter/src/main/java/org/example/jdbc/starter/ConnectionFactory.java
+++ jdbc-starter/src/main/java/org/example/jdbc/starter/ConnectionFactory.java
@@ -1,15 +1,23 @@
 package org.example.jdbc.starter;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionFactory {
     private static Logger logger = LoggerFactory.getLogger(ConnectionFactory.class);
+
+    private final Properties properties;
+
+    public ConnectionFactory(String filename) {
+        this.properties = getDataBaseProperties(filename);
+    }
 
     private Properties getDataBaseProperties(String filename) {
         Properties properties = new Properties();
@@ -34,11 +42,27 @@ public class ConnectionFactory {
         return properties;
     }
 
-    public Connection getConnection() {
-        return null;
+    public Connection getConnection() throws SQLException {
+        MysqlDataSource dataSource = null;
+        try {
+            dataSource = new MysqlDataSource();
+            dataSource.setServerName(properties.getProperty("org.example.jdbc.starter.server"));
+            dataSource.setDatabaseName(properties.getProperty("org.example.jdbc.starter.name"));
+            dataSource.setUser(properties.getProperty("org.example.jdbc.starter.user"));
+            dataSource.setPassword(properties.getProperty("org.example.jdbc.starter.password"));
+            dataSource.setPort(Integer.valueOf(properties.getProperty("org.example.jdbc.starter.port")));
+            dataSource.setAllowMultiQueries(true);
+            dataSource.setServerTimezone("Europe/Warsaw");
+            dataSource.setUseSSL(false);
+            dataSource.setCharacterEncoding("UTF-8");
+        } catch (SQLException e) {
+            logger.error("Error during creating MysqlDataSource", e);
+        }
+        logger.info("Connecting to a selected database...");
+        return dataSource.getConnection();
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws SQLException {
+        Connection connection = new ConnectionFactory("database.properties").getConnection();
     }
 }
