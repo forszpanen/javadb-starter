54107be55183732eae05baef7da71618ebb055e0
adamlagoda <adam_lagoda@interia.pl>
2020-07-16 09:30:32 +0200

    Zadanie 3 - klasa JpaTest

---
+++ jpa-starter/src/main/java/org/example/jpa/starter/lifecycle/JpaTest.java
@@ -0,0 +1,12 @@
+package org.example.jpa.starter.lifecycle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JpaTest {
+    private static final Logger logger = LoggerFactory.getLogger(JpaTest.class);
+
+    public static void main(String[] args) {
+
+    }
+}
