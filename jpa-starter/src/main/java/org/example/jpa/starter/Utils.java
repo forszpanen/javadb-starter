9ebf9270721034d428ce5bd4daa1450702c84cca
adamlagoda <adam_lagoda@interia.pl>
2020-07-16 08:48:08 +0200

    Zadanie 3

---
+++ jpa-starter/src/main/java/org/example/jpa/starter/Utils.java
@@ -0,0 +1,27 @@
+package org.example.jpa.starter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class Utils {
+    private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    public static Date parse(String date) {
+        try {
+            return SIMPLE_DATE_FORMAT.parse(date);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static String dateFormat(Date date) {
+        return SIMPLE_DATE_FORMAT.format(date);
+    }
+
+    public static String dateFormat(LocalDateTime dateTime) {
+        return dateTime.format(DATE_TIME_FORMATTER);
+    }
+}
---
+++ jpa-starter/src/main/java/org/example/jpa/starter/lifecycle/CourseEntityDao.java
@@ -0,0 +1,30 @@
+package org.example.jpa.starter.lifecycle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class CourseEntityDao {
+    private static Logger logger = LoggerFactory.getLogger(CourseEntityDao.class);
+
+    public void save(CourseEntity courseEntity) {
+
+    }
+
+    public void update(CourseEntity courseEntity) {
+
+    }
+
+    public void delete(CourseEntity courseEntity) {
+    }
+
+    public CourseEntity findById(int id) {
+        return null;
+    }
+
+    public List<CourseEntity> list() {
+        return null;
+    }
+
+}
