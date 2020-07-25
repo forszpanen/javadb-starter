1e35de03304e7eeacf5311b422117487a8894a84
adamlagoda <adam_lagoda@interia.pl>
2020-07-26 00:10:55 +0200

    Rozwiązanie zadania 5

--- jdbc-starter/src/main/java/org/example/jdbc/starter/ConnectionFactory.java
+++ jdbc-starter/src/main/java/org/example/jdbc/starter/ConnectionFactory.java
@@ -16,7 +16,7 @@ public class ConnectionFactory {
     private Properties properties;
 
     public ConnectionFactory(String filename) {
-        //this.properties = getDataBaseProperties(filename);
+        this.properties = getDataBaseProperties(filename);
     }
 
     private Properties getDataBaseProperties(String filename) {
@@ -25,7 +25,7 @@ public class ConnectionFactory {
             /**
              * Pobieramy zawartość pliku za pomocą classloadera, plik musi znajdować się w katalogu ustawionym w CLASSPATH
              */
-            ClassLoader classLoader = this.getClass().getClassLoader();
+            ClassLoader classLoader = ConnectionFactory.class.getClassLoader();
             InputStream propertiesStream = classLoader.getResourceAsStream(filename);
             if (propertiesStream == null) {
                 throw new IllegalArgumentException("Can't find file: " + filename);
@@ -44,9 +44,20 @@ public class ConnectionFactory {
 
     public Connection getConnection() throws SQLException {
         MysqlDataSource dataSource = new MysqlDataSource();
-        /*
-        // Przenieś tutaj fragmenty kodu z ConnectionViaDataSource, które parametrzują obiekt DataSource (linijki 25-33
-         */
+        try {
+            dataSource = new MysqlDataSource();
+            dataSource.setServerName(properties.getProperty("org.example.jdbc.starter.server"));
+            dataSource.setDatabaseName(properties.getProperty("org.example.jdbc.starter.name"));
+            dataSource.setUser(properties.getProperty("org.example.jdbc.starter.user"));
+            dataSource.setPassword(properties.getProperty("org.example.jdbc.starter.password"));
+            dataSource.setPort(Integer.valueOf(properties.getProperty("org.example.jdbc.starter.port")));
+            dataSource.setAllowMultiQueries(true);
+            dataSource.setServerTimezone("Europe/Warsaw");
+            dataSource.setUseSSL(false);
+            dataSource.setCharacterEncoding("UTF-8");
+        } catch (SQLException e) {
+            logger.error("Error during creating MysqlDataSource", e);
+        }
         logger.info("Connecting to a selected database...");
         return dataSource.getConnection();
     }
--- library-manager/src/main/java/org/example/library/DatabaseManager.java
+++ library-manager/src/main/java/org/example/library/DatabaseManager.java
@@ -1,5 +1,89 @@
 package org.example.library;
 
-public class DatabaseManager {
+import org.example.jdbc.starter.ConnectionFactory;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseManager {
+    private static final String DATABASE_PROPERTIES_FILE = "library-database.properties";
+    private ConnectionFactory connectionFactory;
+
+    public DatabaseManager() {
+        this.connectionFactory = new ConnectionFactory(DATABASE_PROPERTIES_FILE);
+    }
+
+    public void createDb() throws SQLException {
+        try (Connection connection = connectionFactory.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS library.users (\n" +
+                    "  id INT NOT NULL AUTO_INCREMENT,\n" +
+                    "  login VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_polish_ci NOT NULL,\n" +
+                    "  password VARCHAR(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_polish_ci NOT NULL,\n" +
+                    "  name VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_polish_ci NOT NULL,\n" +
+                    "  admin BOOLEAN NOT NULL,\n" +
+                    "PRIMARY KEY (id))\n" +
+                    "  ENGINE = InnoDB;");
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS library.categories (\n" +
+                    "  id INT NOT NULL AUTO_INCREMENT,\n" +
+                    "  name VARCHAR(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_polish_ci NOT NULL,\n" +
+                    "PRIMARY KEY (id))\n" +
+                    "  ENGINE = InnoDB;");
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS library.books (\n" +
+                    "  id INT NOT NULL AUTO_INCREMENT,\n" +
+                    "  category_id INT NOT NULL,\n" +
+                    "  title VARCHAR(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_polish_ci NOT NULL,\n" +
+                    "  author VARCHAR(70) CHARACTER SET utf8mb4 COLLATE utf8mb4_polish_ci NOT NULL,\n" +
+                    "PRIMARY KEY (id),\n" +
+                    "FOREIGN KEY (category_id)\n" +
+                    "REFERENCES library.categories(id)\n" +
+                    "ON DELETE NO ACTION\n" +
+                    "ON UPDATE NO ACTION)\n" +
+                    "  ENGINE = InnoDB;");
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS library.orders (\n" +
+                    "  id INT NOT NULL AUTO_INCREMENT,\n" +
+                    "  user_id INT NOT NULL,\n" +
+                    "  book_id INT NOT NULL,\n" +
+                    "  order_date DATETIME NOT NULL,\n" +
+                    "  return_date DATETIME NULL,\n" +
+                    "PRIMARY KEY (id),\n" +
+                    "FOREIGN KEY (user_id)\n" +
+                    "REFERENCES library.users(id)\n" +
+                    "ON DELETE NO ACTION\n" +
+                    "ON UPDATE NO ACTION,\n" +
+                    "FOREIGN KEY (book_id)\n" +
+                    "REFERENCES library.books(id)\n" +
+                    "ON DELETE NO ACTION\n" +
+                    "ON UPDATE NO ACTION)\n" +
+                    "  ENGINE = InnoDB;");
+        }
+
+    }
+
+    public void initializeDb() throws SQLException {
+        try (Connection connection = connectionFactory.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate("INSERT INTO categories (name) values ('Systemy operacyjne');");
+        }
+    }
+
+    public void dropDb() throws SQLException {
+        try (Connection connection = connectionFactory.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate("DROP TABLE library.orders;");
+            statement.executeUpdate("DROP TABLE library.books;");
+            statement.executeUpdate("DROP TABLE library.users;");
+            statement.executeUpdate("DROP TABLE library.categories;");
+
+        }
+    }
+
+
+    public static void main(String[] args) throws SQLException {
+        DatabaseManager databaseManager = new DatabaseManager();
+        databaseManager.createDb();
+
+        //databaseManager.dropDb();
+    }
 }
